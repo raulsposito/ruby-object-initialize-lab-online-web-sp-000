86964d4ca73aa42ba73d8ddb568ffa47b6db908f
Raul Sposito <raulsposito@gmail.com>
2019-08-07 00:45:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,3 @@
-#class Dog 
-#  def initialize(dogs_name)
-#    @name = dogs_name
-#  end
-#  
-#  def name 
-#    @name
-#  end
-#    
-#end
-
 class Dog
   def initialize (dogs_name, dogs_breed = "Mutt")
     @name = dogs_name
